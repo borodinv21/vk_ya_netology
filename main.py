4d33ff4f1f699946b68d72e69b0bbd08bd45b53b
borodinv21 <vikc003@mail.ru>
2024-04-15 20:19:46 +0500

    mission completed

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
 import requests
+import json
 from pprint import pprint
 from datetime import datetime
+#from config import vk_access_token, vk_user_id, yandex_access_token - импортирую отдельный файл с токенами, он находится в gitignore
+#Ниже описал куда необходимо вставить токены и id пользователя
+
+
+class YANDEXAPIClient:
+    API_BASE_URL = 'https://cloud-api.yandex.net/v1/disk/'
+
+    def __init__(self, token):
+        self.token = token
+
+
+    def create_folder(self, folder_name):
+        url_resources = 'resources?'
+        params = {
+            'path': folder_name
+        }
+        headers = {
+            'Authorization': f'OAuth {self.token}'
+        }
+
+        response = requests.put(self.API_BASE_URL + url_resources, params=params, headers=headers)
+
+        return response
+
+
+    def get_yandex_upload_photos_response(self, folder_name, image_name):
+        url_resources = 'resources/upload'
+        params = {
+            'path': f"{folder_name}/{image_name}"
+        }
+        headers = {
+            'Authorization': f'OAuth {self.token}'
+        }
+
+        response = requests.get(self.API_BASE_URL + url_resources, params=params, headers=headers)
+
+        return response
+
+    def get_file_info(self, folder_name, image_name):
+        url_resources = 'resources?'
+        params = {
+            'path': f"{folder_name}/{image_name}"
+        }
+        headers = {
+            'Authorization': f'OAuth {self.token}'
+        }
+
+        response = requests.get(self.API_BASE_URL + url_resources, params=params, headers=headers)
+
+        return response
 
 
 class VKAPIClient:
@@ -57,7 +108,68 @@ class VKAPIClient:
 
 
 if __name__ == '__main__':
-    vk_client = VKAPIClient(token=vk_access_token)
-    photos_info_items = vk_client.get_user_photos(user_id)['response']['items']
+    # vk_access_token = Здесь вводите свой токен
+    # vk_user_id = Здесь введите свой id пользователя
+    # yandex_access_token = Здесь введите свой яндекс токен
 
+    data_json = []
+
+    vk_client = VKAPIClient(token=vk_access_token)
+    photos_info_items = vk_client.get_user_photos(vk_user_id)['response']['items']
+    photos_list = vk_client.create_photos_info(photos_info_items)
+    print('Получили доступ к API VK')
+
+    print('Вывожу информацию о фотографиях профиля')
     pprint(vk_client.create_photos_info(photos_info_items))
+
+    ya_client = YANDEXAPIClient(yandex_access_token)
+    print('Получили доступ к API Yandex')
+
+    ya_client.create_folder('Images')
+    print('Создаю папку с именем Images в Яндекс Диске', end='\n'*2)
+
+    for photo in photos_list:
+        print('Считываю информацию о фотографии...')
+
+        response = requests.get(photo['url'])
+        image_name = f"{photo['likes']}.jpg"
+        print(f'Создаю файл с именем {image_name}')
+
+        file_info = ya_client.get_file_info('Images', image_name)
+
+        if file_info.json().get('error') == None:
+            image_name = f"{photo['likes']}_{photo['date']}.jpg"
+            print('Внимание! Файл с таким именем уже существует! Добавляю к имени файла дату загрузки!')
+            print(f'Теперь файл называется {image_name}')
+
+        with open(image_name, 'wb') as file:
+            file.write(response.content)
+            print('Записываю файл на локальный компьютер...')
+
+        yandex_response = ya_client.get_yandex_upload_photos_response('Images', image_name)
+        print('Получил ссылку на загрузку фотографии в Яндекс Диск...')
+
+        with open(image_name, 'rb') as file:
+            try:
+                requests.put(yandex_response.json()['href'], files={'file': file})
+                print(f'Поздравляю! Фотография с именем {image_name} была загружена в папку Images')
+                data_json.append(
+                    {
+                        'file_name': image_name,
+                        'size': photo['size']
+                    }
+                )
+            except:
+                print('Фото с таким именем уже существует! Вы загружаете фотографию, которая уже хранится в папке Images')
+                data_json.append(
+                    {
+                        'file_name': image_name,
+                        'size': photo['size'],
+                        'error': 'Файл уже загружен на Яндекс Диск'
+                    }
+                )
+
+        print('\n')
+
+    with open('data.json', 'w') as file:
+        json.dump(data_json, file)
